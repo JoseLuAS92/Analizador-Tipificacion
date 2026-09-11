51904a4ee5c374d612c82f33e759e5b195fb6872
JoseLuAS92 <alfonsosarmiento_@hotmail.com>
2026-09-11 10:39:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
-function analizar(){
+function analizar() {
 
-const texto =
-document.getElementById("descripcion").value.toLowerCase();
+    const texto =
+    document.getElementById("descripcion").value.toLowerCase();
 
-let resultado = "";
+    let resultado = "";
 
-if(
-texto.includes("neurologia") ||
-texto.includes("migraña") ||
-texto.includes("cefalea")
-){
+    if (
+        texto.includes("neurologia") ||
+        texto.includes("migraña") ||
+        texto.includes("cefalea")
+    ) {
 
-resultado += `
-<div class="card">
-<h3>ESPECIALIDADES MÉDICAS > NEUROLOGÍA</h3>
-<p>Confianza: 95%</p>
-</div>
-`;
+        resultado += `
+        <div class="card">
+            <h3>ESPECIALIDADES MÉDICAS > NEUROLOGÍA</h3>
+            <p>Confianza: 95%</p>
+        </div>
+        `;
 
-}
+    }
 
-if(
-texto.includes("prestador") ||
-texto.includes("ips")
-){
+    if (
+        texto.includes("prestador") ||
+        texto.includes("ips")
+    ) {
 
-resultado += `
-<div class="card">
-<h3>CAMBIO DE PRESTADOR</h3>
-<p>Confianza: 85%</p>
-</div>
-`;
+        resultado += `
+        <div class="card">
+            <h3>CAMBIO DE PRESTADOR</h3>
+            <p>Confianza: 85%</p>
+        </div>
+        `;
 
-}
+    }
 
-if(resultado===""){
+    if (resultado === "") {
 
-resultado = `
-<div class="card">
-No se encontraron coincidencias.
-</div>
-`;
+        resultado = `
+        <div class="card">
+            No se encontraron coincidencias.
+        </div>
+        `;
 
-}
+    }
 
-document.getElementById("resultado").innerHTML = resultado;
+    document.getElementById("resultado").innerHTML = resultado;
 
 }
